1ca2bd7ed2ada1c0b6b1049de3b09a2c66735228
efe <victorefe5@gmail.com>
2023-10-22 15:34:17 +0100

    login and logout works well now

--- website/auth.py
+++ website/auth.py
@@ -1,36 +1,76 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask import session
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>" 
+    session.pop('user', None)
+    session.pop('user_id', None)
+    session.pop('user_email', None)
+    session.pop('user_first_name', None)
+    session.pop('user_last_name', None)
+    session.pop('user_password', None)
+    session.pop('user_created_at', None)
+    session.pop('user_updated_at', None)
+    session.pop('user_deleted_at', None)
+    session.pop('user_is_active', None)
+    session.pop('user_is_admin', None)
+    session.pop('user_is_staff', None)
+    session.pop('user_is_superuser', None)
+    session.pop('user_last_login', None)
+    session.pop('user_date_joined', None)
+    session.pop('user_groups', None)
+    session.pop('user_permissions', None)
+    session.pop('user_is_authenticated', None)
+    session.pop('user_is_anonymous', None)
+    flash('Logged out successfully!', category='success')
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash("Email must be greater than 3 characters", category='error')
-        elif len(firstName) < 2 and len(lastName) < 2:
+        elif len(first_name) < 2 and len(last_name) < 2:
             flash('First name and last name must be greater than 1 character', category='error')
         elif password1 != password2:
             flash("Passwords don't match", category='error')
         elif len(password1) < 7:
             flash("Password must be at least 7 characters", category='error')
         else:
+            new_user = User(email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account created!", category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
