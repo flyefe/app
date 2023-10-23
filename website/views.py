29b07abeb0d7f025507f77807cf065a01665fc25
efe <victorefe5@gmail.com>
2023-10-23 16:03:38 +0100

    project with no javascript

--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from flask import flash
 from . import db
 from .models import Note
 from . import views
-import json
+# import json
 
 views = Blueprint('views', __name__)
 
@@ -28,13 +28,18 @@ def home():
     return render_template("home.html", user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
+@views.route('/delete-note/<int:note_id>', methods=['GET'])
+@login_required
+def delete_note(note_id):
+    note = Note.query.get(note_id)
     if note:
         if note.user_id == current_user.id:
             db.session.delete(note)
             db.session.commit()
-    return jsonify({})
+            flash('Note deleted!', category='success')
+        else:
+            flash("You don't have permission to delete this note.", category='error')
+    else:
+        flash('Note not found.', category='error')
+    
+    return redirect(url_for('views.home'))
